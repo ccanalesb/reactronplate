e49aa422bc820a451fc1a543edd23a5f01127f7c
Marco Torres <mtorres@handband.cl>
2017-06-26 21:37:59 -0400

    Avances en el parser de preguntas, componente Quiz

--- app/src/components/Quiz.jsx
+++ app/src/components/Quiz.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { Row, Col, Affix } from 'antd';
-import { Steps, Button, message } from 'antd';
+import { Row, Col, Affix, Alert } from 'antd';
+import { Steps, Button, message, Card } from 'antd';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import {sample_questions, QuestionUtils} from '../utils.js';
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 const Step = Steps.Step;
 
-const steps = [{
-  title: 'Primera',
-  content: 'First-content',
-}, {
-  title: 'Segunda',
-  content: 'Second-content',
-}, {
-  title: 'Tercera',
-  content: 'Last-content',
-}];
+const steps = sample_questions;
 
 export default class Quiz extends React.Component {
   constructor(props) {
@@ -41,26 +33,53 @@ export default class Quiz extends React.Component {
         <Row type="flex" justify="center" align="middle">
           <Col span={16}>
               <h2 className="display2">
-                <Icon className="colored" type="swap-right" />
-                Pregunta 1
+                <span className="colored smaller">
+                  {this.state.current+1}
+                  <Icon type="swap-right" />
+                </span>
+                {steps[this.state.current].question}
               </h2>
-              <div className="steps-content">{steps[this.state.current].content}</div>
+              {QuestionUtils.get_right_answers_number(steps[this.state.current])>1?
+                <Alert message="Se pueden seleccionar varias opciones" className="colored" type="info" />
+              :""}
+              <div className="steps-content">
+                <Row gutter={16}>
+                  
+                  {steps[this.state.current].answers.map((item, idx) => 
+                    <Col span={8} key={idx}>
+                      <Card bordered={true}>
+                        {item.hasOwnProperty('image')?
+                          <div className="custom-image">
+                            <img alt="example" width="100%" src={item.image} />
+                          </div>
+                        :""}
+                        {item.hasOwnProperty('content')?
+                          <div className="custom-card">
+                            <h3>{item.content}</h3>
+                          </div>
+                        :""}
+                      </Card>
+                    </Col>
+                  )}
+                </Row>
+                {/*{steps[this.state.current].content}*/}
+              </div>
               <div className="steps-action">
                 {
                   this.state.current < steps.length - 1
                   &&
-                  <Button type="primary" onClick={() => this.next()}>Next</Button>
+                  <Button size="large" type="primary" onClick={() => this.next()}>Continuar<Icon type="right" /></Button>
                 }
                 {
                   this.state.current === steps.length - 1
                   &&
-                  <Button type="primary" onClick={() => message.success('Processing complete!')}>Done</Button>
+                  <Button size="large" type="primary" onClick={() => message.success('Processing complete!')}>Done</Button>
                 }
                 {
                   this.state.current > 0
                   &&
-                  <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
-                    Previous
+                  <Button size="large" style={{ marginLeft: 8 }} onClick={() => this.prev()}>
+                    Volver
                   </Button>
                 }
               </div>
@@ -70,7 +89,7 @@ export default class Quiz extends React.Component {
       <Footer>
         <Affix offsetBottom={0}>
           <Steps progressDot current={current}>
-            {steps.map(item => <Step key={item.title} title={item.title} />)}
+            {steps.map((item,index) => <Step key={index} title={"Pregunta "+(index+1)} />)}
             {/*<Step title="Finished" description="This is a description." />
             <Step title="In Progress" description="This is a description." />
             <Step title="Waiting" description="This is a description." />*/}
