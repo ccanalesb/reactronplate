9d05e4f60f8061484a12172404164a5356256d61
ccanalesb <cristian.canales@mail.udp.cl>
2017-06-27 00:20:21 -0400

    Agregando vista de cartas por curso, agregada la lista de alumnos por curso

---
+++ app/src/components/StudentsList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { Table, Icon } from 'antd';
+import {sample_students} from '../utils.js';
+
+export default class StudentsList extends React.Component {
+  render() {
+    let grade_id = parseInt(this.props.match.params.id,10)
+    let students = sample_students[grade_id].students;
+    const columns = [{
+      title: 'Nombre',
+      dataIndex: 'name',
+      key: 'name',
+    }, {
+      title: 'Numero de lista',
+      dataIndex: 'id',
+      key: 'id',
+    }, {
+      title: 'Estado',
+      dataIndex: 'status',
+      key: 'status',
+    }];    
+    return (
+      <div className="body">
+        <Table columns={columns} dataSource={students} />
+      </div>
+    )
+  }
+}
